f4a688648b67bd8e06823d7bfe7a69e61f8c63b3
Kher <etienne.kher@laplateforme.io>
2019-11-28 16:38:39 +0100

    connexion message erreur

--- connexion.php
+++ connexion.php
@@ -30,41 +30,19 @@ include('function.php');
 		{
 			$requete = "SELECT * FROM utilisateurs WHERE login = '".$_POST['login']."';";
 			$resultat = sql($requete);
-			if(!empty($resultat))
-			{
-				if(chiffre($_POST["mdp"])==$resultat["password"])
+			
+				if(chiffre($_POST["mdp"])==$resultat["password"]&&!empty($resultat))
 				{
 					$_SESSION["login"] = $_POST["login"];
 					header("location:index.php");
 				}
 				else
 				{
-					?><p>Votre password n'est pas bon</p>
-					
-					<form action="connexion.php" method="post">
-					<label>Login</label>
-					<input name="login" type="text" required>
-					<label>Password</label>
-					<input name="mdp" type="password" required>
-					<input type="submit" class="mybutton" value="log in">
-					</form>
+					?>
+						<p>une erreur dans le login ou le mot de passe c'est produite</p>
 					<?php
 				}
-			}
-			else
-			{
-				?><p>Votre nom d'utilisateur n'éxiste pas</p>
-				
-				<form action="connexion.php" method="post">
-				<label>Login</label>
-				<input name="login" type="text" required>
-				<label>Password</label>
-				<input name="mdp" type="password" required>
-				<input type="submit" class="mybutton" value="log in">
-				</form>
-				<?php
-			}
-			mysqli_close($connexion);
+			
 		}
 	?>
 </main>
